81289f510d162320bedb62973e1a5becd43a34ac
github username <github email address>
2019-12-02 20:16:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+class Cat{
+    constructor(name, sex) {
+        this.name = name;
+        this.sex =  sex;
+      }
+    
+      speak() {
+        return `${this.name} says meow!`
+      }
+}
+class Dog{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak() { 
+        return `${this.name} says woof!`
+    }
+}
+
+//"It's me! name, the parrot!". If it is not male, speak 
+//returns "name says squawk!".
+class Bird{
+    constructor(name, sex){
+        this.name = name;
+        this.sex = sex;
+    }
+    speak(){
+        if (this.sex == "male"){
+            return `It's me! ${this.name}, the parrot!`
+        }
+        else
+        {
+            return `${this.name} says squawk!`
+        }   
+    }
+
+}
+  
